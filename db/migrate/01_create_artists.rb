d5ca453c130b8b78212258f2e25c34c064ad4019
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-02-21 12:56:29 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -14,20 +14,4 @@ class CreateArtists < ActiveRecord::Migration
     end
   end
 
-  ActiveRecord::Base.establish_connection(
-    :adapter => "sqlite3",
-    :database => "db/artists.sqlite"
-  )
-
-  sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS artist (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      genre TEXT,
-      age INTEGER,
-      hometown TEXT
-    )
-  SQL
-
-  ActiveRecord::Base.connection.execute(sql)
 end
